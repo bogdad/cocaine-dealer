75adabef86ada431e17f698c8c0802deef3f1a00
Rimz <creator@bash.org.ru>
2012-07-04 08:40:49 +0400

    fixed build installation

--- bindings/install_perl_module.cmake
+++ bindings/install_perl_module.cmake
@@ -1,8 +1,1 @@
-#properly install perl module to destination directory
-
-SET(MODULE_TMP_DIR /tmp/perl/modules/cocaine_dealer)
-
-EXECUTE_PROCESS(
-	COMMAND ${CMAKE_COMMAND} -E chdir ${MODULE_TMP_DIR} perl -MExtUtils::Install -e install_default Cocaine/Dealer
-	COMMAND ${CMAKE_COMMAND} -E remove_directory ${MODULE_TMP_DIR}
-	)
+EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E chdir /tmp/perl/modules/cocaine_dealer perl -MExtUtils::Install -e install_default Cocaine/Dealer)
